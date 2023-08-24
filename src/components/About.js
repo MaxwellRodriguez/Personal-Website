e3da668f867b9caeb3d2ef0c7ac8bf5fe631afde
MaxwellRodriguez <66970974+MaxwellRodriguez@users.noreply.github.com>
2023-08-24 17:43:03 -0400

    Mobile Sizing Adjustments Experiments
    also added Melissa into my about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Image} from "antd";
+import AOS from "aos";
 import './About.css';
 
 import MoonBrother from "../assets/MoonBrother.jpg";
-import PortraitSelfie from "../assets/SelfieWPortrait.jpg"
+import PortraitSelfie from "../assets/SelfieWPortrait.jpg";
+
 
 function About() {
+    AOS.init();
     return(
         <div className="About">
             <div>
@@ -18,7 +21,7 @@ function About() {
                     I recently graduated from the University of Florida with a Bachelors in Computer Science-Digital Arts and Sciences. I had a great time learning everything from data structures to design principles, but I think I miss my a cappella group the most.
                 </p>
                 <p>
-                    If I have some spare time, I love playing games with my friends and walking my dog. Games mean a lot to me, and they’re my favorite way of getting to know people. Feel free to challenge me to a game of chess any time.
+                    If I have some spare time, I love playing games with my friends, spending time with my girlfriend, and walking my dog. Games mean a lot to me, and they’re my favorite way of getting to know people. Feel free to challenge me to a game of chess any time.
                 </p>
                 <p>
                     If I have more spare time, my hobbies are cosplay, making art, and game development. Whether it’s creating a cool costume or a neat game concept, it’s really satisfying when one of my ideas comes to life.
